a941440f0e67a92ae0c2535ea90c7671c600a2bb
ashleyannlaz <paperpastels@gmail.com>
2021-09-12 02:52:31 -0400

    Finished MVP

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,49 +1,45 @@
-/*
-  If the user does not have a session saved in the server
+const Users = require("../users/users-model");
 
-  status 401
-  {
-    "message": "You shall not pass!"
+function restricted(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    next({ status: 401, message: "You shall not pass!" });
   }
-*/
-function restricted() {
-
 }
 
-/*
-  If the username in req.body already exists in the database
-
-  status 422
-  {
-    "message": "Username taken"
+async function checkUsernameFree(req, res, next) {
+  try {
+    const existing = await Users.findBy({ username: req.body.username });
+    if (existing.length) {
+      next({ status: 422, message: "Username taken" });
+    } else {
+      next();
+    }
+  } catch (error) {
+    next(error);
   }
-*/
-function checkUsernameFree() {
-
 }
 
-/*
-  If the username in req.body does NOT exist in the database
-
-  status 401
-  {
-    "message": "Invalid credentials"
-  }
-*/
-function checkUsernameExists() {
-
+function checkUsernameExists(req, res, next) {
+  next();
 }
 
-/*
-  If password is missing from req.body, or if it's 3 chars or shorter
-
-  status 422
-  {
-    "message": "Password must be longer than 3 chars"
+async function checkPasswordLength(req, res, next) {
+  try {
+    if (!req.body.password || req.body.password.length < 3) {
+      next({ status: 422, message: "Password must be longer than 3 chars" });
+    } else {
+      next();
+    }
+  } catch (error) {
+    next(error);
   }
-*/
-function checkPasswordLength() {
-
 }
 
-// Don't forget to add these to the `exports` object so they can be required in other modules
+module.exports = {
+  restricted,
+  checkPasswordLength,
+  checkUsernameExists,
+  checkUsernameFree,
+};
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,63 +1,52 @@
-// Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
-// middleware functions from `auth-middleware.js`. You will need them here!
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const { checkPasswordLength, checkUsernameFree } = require("./auth-middleware");
+const Users = require("../users/users-model");
 
-
-/**
-  1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
-
-  response:
-  status 200
-  {
-    "user_id": 2,
-    "username": "sue"
+router.post(
+  "/register",
+  checkUsernameFree,
+  checkPasswordLength,
+  async (req, res, next) => {
+    try {
+      const { username, password } = req.body;
+      const hash = bcrypt.hashSync(password, 8);
+      const user = { username, password: hash };
+      const createdUser = await Users.add(user);
+      res.status(200).json(createdUser);
+    } catch (error) {
+      next(error);
+    }
   }
+);
 
-  response on username taken:
-  status 422
-  {
-    "message": "Username taken"
+router.post("/login", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const [user] = await Users.findBy({ username });
+    if (user && bcrypt.compareSync(password, user.password)) {
+      req.session.user = user;
+      res.status(200).json({ message: `Welcome ${username}` });
+    } else {
+      next({ status: 401, message: "Invalid credentials" });
+    }
+  } catch (error) {
+    next(error);
   }
+});
 
-  response on password three chars or less:
-  status 422
-  {
-    "message": "Password must be longer than 3 chars"
+router.get("/logout", (req, res, next) => {
+  if (req.session.user) {
+    req.session.destroy((error) => {
+      if (!error) {
+        res.status(200).json({ message: "logged out" });
+      } else {
+        next(error);
+      }
+    });
+  } else {
+    res.status(200).json({ message: "no session" });
   }
- */
+});
 
-
-/**
-  2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
-
-  response:
-  status 200
-  {
-    "message": "Welcome sue!"
-  }
-
-  response on invalid credentials:
-  status 401
-  {
-    "message": "Invalid credentials"
-  }
- */
-
-
-/**
-  3 [GET] /api/auth/logout
-
-  response for logged-in users:
-  status 200
-  {
-    "message": "logged out"
-  }
-
-  response for not-logged-in users:
-  status 200
-  {
-    "message": "no session"
-  }
- */
-
- 
-// Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router;
